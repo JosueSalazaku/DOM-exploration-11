ce627afdfb36e3183af434d1ef6832bb35830abe
Josué Salazaku <josuesalazaku@gmail.com>
2023-10-31 09:43:43 +0100

    Improve create element function

--- 3-Creating nodes/script.js
+++ 3-Creating nodes/script.js
@@ -1,17 +1,72 @@
 const modifyArticle = document.querySelector("article");
-const groups = ["Kobe", "Manu", "Kyrie", "Bron"];
+const groups = [
+	"Lucas",
+	"Kilian",
+	"Alec",
+	"Mohamed",
+	"Pieter",
+	"Rebecca",
+	"Alessandro",
+	"Josué",
+	"Thibault",
+	"Eduarda",
+	"Mohammed",
+	"Funda",
+	"Luis",
+	"Alexandru",
+	"Andrej",
+	"Danté",
+	"jonasi",
+	"Kelsey",
+	"Rana",
+	"Jana",
+	"Sieglinde",
+	"Gustave",
+	"Sezin",
+	"Thierry",
+	"Sylvie",
+	"Anaïs",
+];
 
 function getRandomColor() {
-  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+	return "#" + Math.floor(Math.random() * 16777215).toString(16);
 }
 
 for (const group of groups) {
-  const section = document.createElement("section");
-  section.style.backgroundColor = getRandomColor();
+	const section = document.createElement("section");
+	section.style.backgroundColor = getRandomColor();
 
-  const paragraph = document.createElement("p");
-  paragraph.textContent = groups;
+	const paragraph = document.createElement("p");
+	paragraph.textContent = group;
 
-  section.appendChild(paragraph);
-  modifyArticle.appendChild(section);
+	section.appendChild(paragraph);
+	modifyArticle.appendChild(section);
 }
+
+function getRandomOrder() {
+	return Math.random() - 0.5;
+}
+
+function getContrastColor(hexColor) {
+	const r = parseInt(hexColor.substr(1, 2), 16);
+	const g = parseInt(hexColor.substr(3, 2), 16);
+	const b = parseInt(hexColor.substr(5, 2), 16);
+	const brightness = (r * 299 + g * 587 + b * 114) / 1000;
+
+	return brightness > 128 ? "#000000" : "#FFFFFF";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+	const modifyArticle = document.querySelector("article");
+	const sections = Array.from(modifyArticle.querySelectorAll("section"));
+
+	sections.sort(getRandomOrder);
+
+	sections.forEach((section) => {
+		const bgColor =
+			getComputedStyle(section).getPropertyValue("background-color");
+		const contrastColor = getContrastColor(bgColor);
+		section.style.color = contrastColor;
+		modifyArticle.appendChild(section);
+	});
+});
