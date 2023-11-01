0b23b0595265d92f9f9b78213b9141b98da6f8f2
Josué Salazaku <josuesalazaku@gmail.com>
2023-11-01 22:21:48 +0100

    Move the h2 elment in the 3rd section

--- 4-Navigate/script.js
+++ 4-Navigate/script.js
@@ -3,3 +3,18 @@ const a = ol.children[4]; // the last child
 const b = ol.children[0]; // the first child
 
 ol.insertBefore(a, b);
+
+const section3 = document.getElementById("section3");
+const section2 = document.getElementById("section2");
+
+const h2InSection2 = section2.querySelector("h2"); // selecting the h2 elements inside the different sections
+const h2InSection3 = section3.querySelector("h2");
+
+section2.appendChild(h2InSection3);
+section3.appendChild(h2InSection2);
+
+// console.log(section3.children[0]);
+// console.log(section2.children[0]);
+
+const lastSection = document.querySelector("section:last-child");
+lastSection.remove();
