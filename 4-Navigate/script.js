26cc872342273b6f338383346811116021217329
Josué Salazaku <josuesalazaku@gmail.com>
2023-11-01 21:43:40 +0100

    Move the the a to the top of the list

--- 4-Navigate/script.js
+++ 4-Navigate/script.js
@@ -1,3 +1,5 @@
 const ol = document.querySelector("ol");
-const a = ol.children[4];
-const b = ol.children[0];
+const a = ol.children[4]; // the last child
+const b = ol.children[0]; // the first child
+
+ol.insertBefore(a, b);
