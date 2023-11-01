eed818ff5740272a14a4631ebaa7015fd0f95688
Josué Salazaku <josuesalazaku@gmail.com>
2023-11-01 18:13:22 +0100

    Improve eventlistener to change p element color depending on bg

--- 3-Creating nodes/script.js
+++ 3-Creating nodes/script.js
@@ -1,3 +1,4 @@
+//Array of names
 const article = document.querySelector("article");
 let groups = [
 	"Lucas",
@@ -28,46 +29,53 @@ let groups = [
 	"Anaïs",
 ];
 
+//This function generates a random color represented as a hexadecimal value
 function getRandomColor() {
 	return "#" + Math.floor(Math.random() * 16777215).toString(16);
 }
 
-for (const group of groups) {
-	const section = document.createElement("section");
-	section.style.backgroundColor = getRandomColor();
-
-	const paragraph = document.createElement("p");
-	paragraph.textContent = group;
-
-	section.appendChild(paragraph);
-	article.appendChild(section);
-}
-
+// This function is used for sorting the sections randomly
 function getRandomOrder() {
 	return Math.random() - 0.5;
 }
 
+/*This function calculates the contrast color (either black or white) based on the brightness of the provided hex color.*/
 function getContrastColor(hexColor) {
 	const r = parseInt(hexColor.substr(1, 2), 16);
 	const g = parseInt(hexColor.substr(3, 2), 16);
 	const b = parseInt(hexColor.substr(5, 2), 16);
 	const brightness = (r * 299 + g * 587 + b * 114) / 1000;
 
+	// console.log(getRandomColor());
 	return brightness > 128 ? "#000000" : "#FFFFFF";
-	console.log(getRandomColor());
 }
 
+// This event listener waits for the DOM content to be fully loaded before executing the provided function.
 document.addEventListener("DOMContentLoaded", function () {
-	const article = document.querySelector("article");
+	/* This loop iterates through the list of names (groups). For each name and creates section element, 
+sets random bg-color for section , creates p element ans appens p to section and append section to article element */
+	for (const group of groups) {
+		const section = document.createElement("section");
+		const bgColor = getRandomColor();
+
+		section.style.backgroundColor = bgColor;
+
+		const paragraph = document.createElement("p");
+		paragraph.textContent = group;
+
+		const contrastColor = getContrastColor(bgColor);
+
+		paragraph.style.color = contrastColor;
+
+		section.appendChild(paragraph);
+		article.appendChild(section);
+	}
+
 	const sections = Array.from(article.querySelectorAll("section"));
 
 	sections.sort(getRandomOrder);
 
 	sections.forEach((section) => {
-		const bgColor =
-			getComputedStyle(section).getPropertyValue("background-color");
-		const contrastColor = getContrastColor(bgColor);
-		section.style.color = contrastColor;
 		article.appendChild(section);
 	});
 });
