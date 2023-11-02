2dd74166dbaf8d40bf09d989dd194468f5f45461
Josué Salazaku <josuesalazaku@gmail.com>
2023-11-02 11:22:39 +0100

    Improve doument children selection

--- 4-Navigate/script.js
+++ 4-Navigate/script.js
@@ -4,19 +4,20 @@ const b = ol.children[0]; // the first child
 
 ol.insertBefore(a, b);
 
-const section3 = document.getElementById("section3");
-const section2 = document.getElementById("section2");
+// const section3 = document.getElementById("section3");
+// const section2 = document.getElementById("section2");
 
-const h2InSection2 = section2.querySelector("h2"); // selecting the h2 elements inside the different sections
-const h2InSection3 = section3.querySelector("h2");
+const section3 = document.body.children[1].children[1];
+const section2 = document.body.children[1].children[2];
 
-section2.appendChild(h2InSection3);
-section3.appendChild(h2InSection2);
+const h2InSection2 = section2.children[0].children[0];
+const h2InSection3 = section3.children[0];
 
-// console.log(section3.children[0]);
-// console.log(section2.children[0]);
+// section2.appendChild(h2InSection3);
+// section3.appendChild(h2InSection2);
+
+console.log(section3.children[0]);
+console.log(section2.children[0].children[1]);
 
 const lastSection = document.querySelector("section:last-child");
 lastSection.parentElement.removeChild(lastSection);
-
-console.log(lastSection);
